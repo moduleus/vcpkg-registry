0b9563eb24fa715e839fa66ba5c60813562ff0bc
Vincent Le Garrec <vincent.legarrec@moduleus.com>
2025-01-22 12:16:00 +0100

    [urx] v1.1.0

---
+++ ports/urx/1.1.0/portfile.cmake
@@ -0,0 +1,65 @@
+vcpkg_minimum_required(VERSION 2023-08-09)
+
+set(REF_VALUE 97460fd11d8e04d1a7c7b2322b7a0e33286387d9)
+set(SHA1_VALUE
+    79a20bceac4c4fcec9670a2097b0054ed32c8d4b0723474481f5b813da9609692ab50f10da70b36d82b307f9fcd006e6f61a6555f14cdc9b2745564f8af650a9
+)
+
+vcpkg_from_github(
+  OUT_SOURCE_PATH
+  SOURCE_PATH
+  REPO
+  moduleus/urx
+  REF
+  ${REF_VALUE}
+  SHA512
+  ${SHA1_VALUE}
+  HEAD_REF
+  main)
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)
+set(suffix "")
+if(${BUILD_STATIC})
+  set(suffix "_static")
+endif()
+
+vcpkg_check_features(
+  OUT_FEATURE_OPTIONS
+  FEATURE_OPTIONS
+  FEATURES
+  hdf5
+  WITH_HDF5
+  python
+  WITH_PYTHON
+  python-wheel
+  WITH_PYTHON_WHL
+  tests
+  BUILD_TESTING
+  matlab
+  WITH_MATLAB)
+
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS ${FEATURE_OPTIONS})
+vcpkg_cmake_install()
+
+if(${REF_VALUE} STREQUAL ${VERSION})
+  vcpkg_cmake_config_fixup(PACKAGE_NAME "urx" CONFIG_PATH
+                           "lib/cmake/Urx-${VERSION}${suffix}")
+else()
+  file(GLOB ALL_VERSION_PATH "${CURRENT_PACKAGES_DIR}/lib/cmake/Urx-*${suffix}")
+  foreach(dir ${ALL_VERSION_PATH})
+    file(RELATIVE_PATH relative_dir ${CURRENT_PACKAGES_DIR} ${dir})
+    vcpkg_cmake_config_fixup(PACKAGE_NAME "urx" CONFIG_PATH "${relative_dir}")
+  endforeach()
+endif()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/license")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/license")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/License.txt")
+
+if("matlab" IN_LIST FEATURES)
+  if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    set(VCPKG_POLICY_DLLS_IN_STATIC_LIBRARY enabled)
+  endif()
+endif()
