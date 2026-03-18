daee5da2ef2f07aae1fa7209c70a590f578e085d
Vincent Le Garrec <vincent.legarrec@moduleus.com>
2026-03-18 12:47:09 +0100

    [urx] v1.4.0

---
+++ ports/urx/1.4.0/portfile.cmake
@@ -0,0 +1,100 @@
+if("use-system-libs" IN_LIST FEATURES)
+  set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+  return()
+endif()
+
+set(REF_VALUE f30fcbfda58e181b3b70b85579c8a3414a1ec61d)
+set(SHA1_VALUE 3b552b63735d43fc2917939f36a1cfb0015f572d3cc5258dde0426c7e955358c91e0479a0e700f3adcd460156552893206fc9ddbeb1c8a67d4990fb79be3b7b3
+)
+
+vcpkg_from_github(
+  OUT_SOURCE_PATH
+  SOURCE_PATH
+  REPO
+  moduleus/urx
+  REF
+  ${REF_VALUE}
+  SHA512
+  ${SHA1_VALUE}
+  HEAD_REF
+  main)
+
+set(VCPKG_REGISTRY_REF_VALUE 654065cc091b4e21e87f69c8a60eb11fa52e1768)
+set(VCPKG_REGISTRY_SHA1_VALUE 60339babd8323134006a4fd6ec9f418552b04bc2de38df0331bebaacf4bd4b2c5bce1a5e870fd790bba1fa4e5e9057d592e9ead78542096643a0abaf815e8048
+)
+
+vcpkg_from_github(
+  OUT_SOURCE_PATH
+  SOURCE_PATH_VCPKG_REGISTRY
+  REPO
+  moduleus/vcpkg-registry
+  REF
+  ${VCPKG_REGISTRY_REF_VALUE}
+  SHA512
+  ${VCPKG_REGISTRY_SHA1_VALUE}
+  HEAD_REF
+  main)
+
+file(REMOVE ${SOURCE_PATH}/vcpkg-registry)
+file(COPY ${SOURCE_PATH_VCPKG_REGISTRY}/
+     DESTINATION ${SOURCE_PATH}/vcpkg-registry)
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)
+set(suffix "")
+if(${BUILD_STATIC})
+  set(suffix "_static")
+endif()
+
+vcpkg_check_features(
+  OUT_FEATURE_OPTIONS
+  FEATURE_OPTIONS
+  FEATURES
+  hdf5
+  WITH_HDF5
+  python
+  WITH_PYTHON
+  python-wheel
+  WITH_PYTHON_WHL
+  tests
+  BUILD_TESTING
+  matlab
+  WITH_MATLAB)
+
+string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" CRT_STATIC)
+if(CRT_STATIC)
+  set(CRT_SHARED_LIBS "-DCRT_SHARED_LIBS:BOOL=OFF")
+else()
+  set(CRT_SHARED_LIBS "-DCRT_SHARED_LIBS:BOOL=ON")
+endif()
+
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS ${FEATURE_OPTIONS}
+                      ${CRT_SHARED_LIBS})
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+
+if(${REF_VALUE} STREQUAL ${VERSION})
+  vcpkg_cmake_config_fixup(PACKAGE_NAME "urx" CONFIG_PATH
+                           "lib/cmake/Urx-${VERSION}${suffix}")
+else()
+  file(GLOB ALL_VERSION_PATH "${CURRENT_PACKAGES_DIR}/lib/cmake/Urx-*${suffix}")
+  foreach(dir ${ALL_VERSION_PATH})
+    file(RELATIVE_PATH relative_dir ${CURRENT_PACKAGES_DIR} ${dir})
+    vcpkg_cmake_config_fixup(PACKAGE_NAME "urx" CONFIG_PATH "${relative_dir}")
+  endforeach()
+endif()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/license")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/license")
+
+# Don't delete debug/share. whl will be installed here. Need Python scripts for
+# tests ?
+set(VCPKG_POLICY_ALLOW_DEBUG_SHARE enabled)
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/License.txt")
+
+if("matlab" IN_LIST FEATURES)
+  if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    set(VCPKG_POLICY_DLLS_IN_STATIC_LIBRARY enabled)
+  endif()
+endif()
